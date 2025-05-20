695ede041763312d05a2439cedca611fd43ac18d
IzayoiS <iqbalmhasby54@gmail.com>
2025-05-20 10:15:54 +0700

    fix: cors typo

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-        AllowOrigins: "http://localhost:3000, https://portfolio-iqbals.vercel.app/", 
+        AllowOrigins: "http://localhost:3000, https://portfolio-iqbals.vercel.app", 
         AllowHeaders: "Origin, Content-Type, Accept, Authorization",
     }))
 
